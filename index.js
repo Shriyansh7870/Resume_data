27f6fc34fdfcf8519a101e86d5932ca6929a54e7
Shriyansh Kumar <71579575+Shriyansh7870@users.noreply.github.com>
2024-10-22 03:57:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 \usepackage{url}
 \usepackage{multicol}
 \usepackage{tabularx}
+\usepackage{fontawesome}
 \setlength{\multicolsep}{-3.0pt}
 \setlength{\columnsep}{-1pt}
 
@@ -28,10 +29,10 @@
 %----------CONTACT DETAILS----------
 \begin{center}
     {\Huge \scshape Shriyansh Kumar} \\ \vspace{1pt}
-    +91 9351864035 ~ \href{mailto:Shriyansh7870@gmail.com}{Shriyansh4035@gmail.com} \\
-    \href{https://www.linkedin.com/in/shri7870/}{linkedin.com/in/Shriyansh} ~
-    \href{https://github.com/Shriyansh7870}{github.com/Shriyansh7870} ~
-    \href{https://next-portofoli-git-main-shriyansh-kumars-projects.vercel.app/}{Portfolio}
+   \faPhone\ +91 9351864035 ~ \href{mailto:Shriyansh7870@gmail.com}\faEnvelope\ {Shriyansh7870@gmail.com} \\
+   \faLinkedin\ \href{https://www.linkedin.com/in/shri7870/}{linkedin.com/in/Shriyansh} ~
+   \faGithub\  \href{https://github.com/Shriyansh7870}{github.com/Shriyansh7870} ~
+   {\faFolderOpen} \href{https://next-portofoli-git-main-shriyansh-kumars-projects.vercel.app/}{Portfolio}
     \vspace{-8pt}
 \end{center}
 
@@ -41,59 +42,55 @@
     \item \textbf{Languages \& Frameworks:} JavaScript, ReactJs, NextJS, NodeJs, Express, MongoDB
     \item \textbf{Tools \& Technologies:} Web Development, GitHub, Webpack, Vite, Parcel, GitHub Actions, Netlify, Vercel, REST, GraphQL, Firebase
 \end{itemize}
-
 %-----------Work Experience-----------
 \section{Work Experience}
 \begin{itemize}[leftmargin=0.15in, label={}]
-    \item \textbf{Full Stack Developer} \hfill \textbf{Nov 2023 - Present} \\
-    \textbf{Umenit Solution LLP, Gurugram, India}
+    \item \textbf{Full Stack Developer}, \textbf{(Umenit Solution LLP, India)} \hfill \textbf{Nov 2023 - Oct 2024 }
     \begin{itemize}[leftmargin=0.15in, label={}]
-        \item \textbf{Umenit AI Bot } \hfill \href{https://agent.umenit.ai/auth/signin}{\underline{View Live Project}} \\
-    Developed and integrated AI-based Umenit Bots into a website, increasing user engagement by 40\%.
-         \item \textbf{Parking Arms  } \hfill \href{https://devapp.swiftstay.com/}{\underline{View Live Project}} \\
-   Led the development of Parking Arms project using Next.js, TypeScript, and Tailwind CSS, improving website
-performance and user base by 60\%.
+        \item \textbf{Umenit AI Bot } \hfill \href{https://agent.umenit.ai/auth/signin}{\textit{\faLink}} \\
+        \textbullet \ Developed a robust algorithm to integrate Umenit Bots, achieving over 3 hours of weekly time savings on support queries per team member while enabling users to access information seamlessly without assistance. \vspace{0.1cm} % Add space after the bullet point
 
-      \item \textbf{LinkStaf} \hfill \href{https://dev.linkhealthstaff.com/}{\underline{View Live Project}} \\
-   Built health management systems with React.js and Tailwind CSS, utilizing Redux for state management and
-GraphQL for API interaction
+        \item \textbf{Parking Arms } \hfill \href{https://devapp.swiftstay.com/}{\textit{\faLink}} \\
+        \textbullet \ Directed the successful launch of the Parking Arms project using Next.js, TypeScript, and Tailwind CSS; this initiative streamlined user interactions and improved site load time by an impressive 35\%, enhancing overall usability. \vspace{0.1cm} % Add space after the bullet point
+        
+        \item \textbf{LinkStaf} \hfill \href{https://dev.linkhealthstaff.com/}{\textit{\faLink}} \\
+        \textbullet \ Built a robust health management system, employing modern front-end technologies for a responsive user interface and leveraging state management libraries for optimized performance. \\
+        \textbullet \ Engineered a multi-layered authentication process for the health management platform, enhancing user trust while achieving zero unauthorized access incidents over six months of operation through rigorous testing and monitoring practices. \vspace{0.1cm} % Add space after the bullet point
+
     \end{itemize}
     
-    \item \textbf{Full Stack Developer Intern} \hfill \textbf{Sep 2022 - Apr 2023} \\
-    \textbf{AICT Pvt Ltd, Durgapur, India}
+    \item \textbf{Full Stack Developer Intern},\textbf{(AICT Pvt Ltd India)} \hfill \textbf{Sep 2022 - Apr 2023} \vspace{0.1cm} % Add space after the item
     \begin{itemize}[leftmargin=0.15in, label={}]
-        \item Designed a full-featured e-commerce website, improving user engagement by 30\% through efficient product management and secure user authentication.
-        \item Leveraged MongoDB for database management and Express.js for backend development, ensuring a scalable and responsive web application.
+    \textbullet \ Developed a sophisticated e-commerce website that enhanced user engagement by 30\% through streamlined product management and robust user authentication. \vspace{0.1cm}\\ % Add space after the bullet point
+
+        \item \textbullet \ Employed advanced MongoDB for database management and utilized Express.js for backend development, ensuring a scalable and high-performance web application. \vspace{0.1cm} % Add space after the bullet point
+
     \end{itemize}
 \end{itemize}
-
 %-----------Relevant Projects-----------
 \section{Projects}
 \begin{itemize}[leftmargin=0.15in, label={}]
-    \item \textbf{Udemy Clone} \hfill \href{https://sparkling-dasik-8e2846.netlify.app/}{\underline{View Live Project}} \\
-    Built a Udemy clone with course management, user authentication, and payment processing features, resulting in a 25\% increase in user retention.
+  \item \textbf{Udemy Clone} \hfill \href{https://sparkling-dasik-8e2846.netlify.app/}{\textit{\faLink}} \\
+\textbullet \ Created a comprehensive Udemy clone with advanced course management, user authentication, and secure payment processing, leading to a 25\% boost in user retention. \\
+\textbullet \ Implemented a modern and responsive UI framework, facilitating easy navigation for over 7,000 users, while decreasing support queries by 30\% 
 
-    \item \textbf{E-Commerce Platform} \hfill \href{https://jolly-marzipan-656fc0.netlify.app/}{\underline{View Live Project}} \\
-    Developed a feature-rich e-commerce platform using the MERN stack, enhancing user experience and enabling scalability.
+ \item \textbf{E-Commerce Platform} \hfill \href{https://jolly-marzipan-656fc0.netlify.app/}{\textit{\faLink}} \\
+\textbullet \ Constructed a robust e-commerce platform using the MERN stack, boosting sales by 30\% and reducing load times by 25\%, significantly enhancing user experience. \\
+\textbullet \ Implemented secure payment processing and user authentication with JWT, ensuring a safe and seamless shopping experience.
 
-    \item \textbf{Resume Filter Bot} \\
-    Created a resume filter bot that analyzes resumes based on job descriptions, streamlining the hiring process by identifying best-fit candidates.
+
 \end{itemize}
+
 
 %-----------Education-----------
 \section{Education}
 \begin{itemize}[leftmargin=0.15in, label={}]
-    \item \textbf{B.Tech in Information Technology} \hfill \textbf{2019 - 2023} \\
-    Bengal College of Engineering and Technology, Durgapur \\
-    CGPA: 8.9/10.
+    \item \textbf{Bengal College of Engineering and Technology} \hfill \textbf{ Durgapur India } \\
+  B.Tech in Information Technology,
+    CGPA: 8.9/10.\hfill \textbf{ June 2019 - May 2023}
 \end{itemize}
 
-%-----------Certifications-----------
-\section{Certifications}
-\begin{itemize}[leftmargin=0.15in, label={}]
-    \item Full Stack Development Program, PrepByte Academy
-   
-\end{itemize}
+
 
 %-----------Extracurricular-----------
 
